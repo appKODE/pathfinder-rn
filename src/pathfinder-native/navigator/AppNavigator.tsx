88af6182e5d78a859bd7f15c2a5c8760958c2382
Чернов Александр <ca@kode.ru>
2021-08-13 16:19:42 +0300

    refactor(ui): refactoring ui

--- src/pathfinder-native/navigator/AppNavigator.tsx
+++ src/pathfinder-native/navigator/AppNavigator.tsx
@@ -4,10 +4,11 @@ import { Assets } from '../assets';
 import { Tab, Tabs } from '../features/navigation';
 import { HomePage } from '../flows/home';
 import { SettingsPage } from '../flows/settings';
+import { theme } from '../ui/theme';
 
 export const AppNavigator: React.FC = () => {
   return (
-    <Tabs>
+    <Tabs activeColor={theme.colors.foreground.primary}>
       <Tab routeName={'home'} icon={Assets.icons.home} label="Home">
         <HomePage />
       </Tab>
